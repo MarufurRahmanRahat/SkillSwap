94afbd5c0e7725008afa9978fed4bf971de84a7f
MarufurRahmanRahat <marufrahat88@gmail.com>
2025-10-22 22:24:10 +0600

    firebase authentication initialized

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-lW5kS4zjZDb0JNLQTnGACD4z8Wp55vI",
+  authDomain: "ph-assignment-9-12482.firebaseapp.com",
+  projectId: "ph-assignment-9-12482",
+  storageBucket: "ph-assignment-9-12482.firebasestorage.app",
+  messagingSenderId: "755894275901",
+  appId: "1:755894275901:web:8af6c7ee6c3c76f19d7f54"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
